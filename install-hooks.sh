9637f867ea2c016843442307179566d6c1b08e49
Jacob Wasserman <jwasserman@gmail.com>
2017-05-03 10:05:38 -0400

    Initial commit

---
+++ install-hooks.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+basedir=$(git rev-parse --show-toplevel)
+repohookspath="$basedir/.git/hooks/"
+localhook="$basedir/hooks/pre-commit"
+
+repohook="$repohookspath/pre-commit"
+
+echo "Creating symlink $repohook to $localhook"
+ln -si $localhook $repohook
